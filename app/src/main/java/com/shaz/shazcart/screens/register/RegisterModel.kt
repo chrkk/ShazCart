d8f98eb3e5dff4b5ec838f103a476649c39ebfec
chrkk <christianjake.alalim@cit.edu>
2026-05-22 00:39:54 +0800

    added a mode in register model

--- app/src/main/java/com/shaz/shazcart/screens/register/RegisterModel.kt
+++ app/src/main/java/com/shaz/shazcart/screens/register/RegisterModel.kt
@@ -4,10 +4,10 @@ import com.shaz.shazcart.app.CustomApp
 import com.shaz.shazcart.data.User
 
 class RegisterModel(private val app: CustomApp) : RegisterContract.Model {
-    override fun registerUser(name: String, email: String, password: String): Boolean {
+    override fun registerUser(name: String, email: String, password: String, mode: String): Boolean {
         return if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
-            app.setUser(User(email, password))   // store new user
-            app.setLoggedIn(false)               // not logged in yet
+            app.setUser(User(email, password, mode)) // Store user with Mode
+            app.setLoggedIn(false)
             true
         } else {
             false
