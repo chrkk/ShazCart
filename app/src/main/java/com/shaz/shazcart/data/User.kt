72f73ecf1df1b5dc9e309648acea475cdafb94ab
chrkk <christianjake.alalim@cit.edu>
2026-05-22 00:37:30 +0800

    added solo or group mode

--- app/src/main/java/com/shaz/shazcart/data/User.kt
+++ app/src/main/java/com/shaz/shazcart/data/User.kt
@@ -1,5 +1,7 @@
 package com.shaz.shazcart.data
 
-data class User (var username: String = "", var password: String = ""){
-
-}
+data class User(
+    var username: String = "",
+    var password: String = "",
+    var mode: String = "Group" // "Group" or "Solo"
+)
