5c5a00a43f21dd0b06b3b8123b80fec202cb3b93
chrkk <christianjake.alalim@cit.edu>
2026-05-22 00:59:35 +0800

    added group mode

--- app/src/main/java/com/shaz/shazcart/data/User.kt
+++ app/src/main/java/com/shaz/shazcart/data/User.kt
@@ -3,5 +3,6 @@ package com.shaz.shazcart.data
 data class User(
     var username: String = "",
     var password: String = "",
-    var mode: String = "Group" // "Group" or "Solo"
+    var mode: String = "Group",
+    var budgetLimit: Double = 0.0 // Add this field
 )
